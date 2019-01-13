0d8db092716fe314d00a3af877ca61f302c94782
n490808114 <n490808114@users.noreply.github.com>
2019-01-13 10:50:06 +0800

    add

---
+++ traning/FloorMod.java
@@ -0,0 +1,8 @@
+public class FloorMod{
+    public static void main(String[] args) {
+        int s = Math.floorMod(-21,-5);
+        System.out.println("FloorMod "+s);
+        int s1 = -21 % -5;
+        System.out.println("% "+s1);
+    }
+}
